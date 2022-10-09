f1334564cd26b9f3157924cde4c0bf06b59b07da
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-09 07:08:38 -1000

    Adds more to pseudo code

--- script.js
+++ script.js
@@ -15,30 +15,36 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
+
 }
+
+
+
+
+
 
 ///////// pseudo code////////
 /*
 1. Show prompt asking to generate a password (UI)
-user chooses generate password (event)
+2 user chooses generate password (event)
 HANDLE EVENT
-store value in userChoice (UI)
-prompt password criteria: password character length (8-128max)? (UI)
+3 store value in userChoice (UI)
+4 prompt password criteria: password character length (8-128max)? (UI)
   if user chooses number then choose number of characters
-prompt character types:
-  prompt include lowercase? (UI)
+5 prompt character types:
+6 prompt include lowercase? (UI)
     if yes then include lowercase
     if no then move on
-  prompt include uppercase?
+7 prompt include uppercase?
     if yes then include
     if no then move on
-  prompt numeric?
+8 prompt numeric?
     if yes then include
     if no then move on
-  prompt special characters?
+9 prompt special characters?
     if yes include
     if no move on
-    
+10 Alert user of new password?    
 
 after each prompt, input should be validated
 password generated and displayed in alert or on page
