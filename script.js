24fc45016004c6469beb2f0f53eef4c7a8937d2b
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-09 08:42:48 -1000

    Fixed DO NOT CHANGE area of JavaScript because of order error

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  passwordText.value = password;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
